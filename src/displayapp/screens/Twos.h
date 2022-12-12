9d85d5404a07095605a721bacfc208299ce4fbca
Nhan Mai <nhan.mai@rtrobotics.com>
2022-12-12 19:05:12 +0700

    disable TWOS

--- src/displayapp/screens/Twos.h
+++ src/displayapp/screens/Twos.h
@@ -1,38 +1,38 @@
-#pragma once
+// #pragma once
 
-#include <lvgl/src/lv_core/lv_obj.h>
-#include "displayapp/screens/Screen.h"
+// #include <lvgl/src/lv_core/lv_obj.h>
+// #include "displayapp/screens/Screen.h"
 
-namespace Pinetime {
-  namespace Applications {
-    struct TwosTile {
-      bool merged = false;
-      unsigned int value = 0;
-    };
-    namespace Screens {
-      class Twos : public Screen {
-      public:
-        Twos(DisplayApp* app);
-        ~Twos() override;
+// namespace Pinetime {
+//   namespace Applications {
+//     struct TwosTile {
+//       bool merged = false;
+//       unsigned int value = 0;
+//     };
+//     namespace Screens {
+//       class Twos : public Screen {
+//       public:
+//         Twos(DisplayApp* app);
+//         ~Twos() override;
 
-        bool OnTouchEvent(TouchEvents event) override;
+//         bool OnTouchEvent(TouchEvents event) override;
 
-      private:
-        static constexpr int nColors = 5;
-        lv_style_t cellStyles[nColors];
+//       private:
+//         static constexpr int nColors = 5;
+//         lv_style_t cellStyles[nColors];
 
-        lv_obj_t* scoreText;
-        lv_obj_t* gridDisplay;
-        static constexpr int nCols = 4;
-        static constexpr int nRows = 4;
-        static constexpr int nCells = nCols * nRows;
-        TwosTile grid[nRows][nCols];
-        unsigned int score = 0;
-        void updateGridDisplay();
-        bool tryMerge(int newRow, int newCol, int oldRow, int oldCol);
-        bool tryMove(int newRow, int newCol, int oldRow, int oldCol);
-        bool placeNewTile();
-      };
-    }
-  }
-}
+//         lv_obj_t* scoreText;
+//         lv_obj_t* gridDisplay;
+//         static constexpr int nCols = 4;
+//         static constexpr int nRows = 4;
+//         static constexpr int nCells = nCols * nRows;
+//         TwosTile grid[nRows][nCols];
+//         unsigned int score = 0;
+//         void updateGridDisplay();
+//         bool tryMerge(int newRow, int newCol, int oldRow, int oldCol);
+//         bool tryMove(int newRow, int newCol, int oldRow, int oldCol);
+//         bool placeNewTile();
+//       };
+//     }
+//   }
+// }
