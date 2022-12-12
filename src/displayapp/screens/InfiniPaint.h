a57c96fd2b7dd0c82ee528509204fe1fbd8646ea
Nhan Mai <nhan.mai@rtrobotics.com>
2022-12-12 19:18:04 +0700

    disable drawing feature

--- src/displayapp/screens/InfiniPaint.h
+++ src/displayapp/screens/InfiniPaint.h
@@ -1,38 +1,38 @@
-#pragma once
+// #pragma once
 
-#include <lvgl/lvgl.h>
-#include <cstdint>
-#include <algorithm> // std::fill
-#include "displayapp/screens/Screen.h"
-#include "components/motor/MotorController.h"
+// #include <lvgl/lvgl.h>
+// #include <cstdint>
+// #include <algorithm> // std::fill
+// #include "displayapp/screens/Screen.h"
+// #include "components/motor/MotorController.h"
 
-namespace Pinetime {
-  namespace Components {
-    class LittleVgl;
-  }
-  namespace Applications {
-    namespace Screens {
+// namespace Pinetime {
+//   namespace Components {
+//     class LittleVgl;
+//   }
+//   namespace Applications {
+//     namespace Screens {
 
-      class InfiniPaint : public Screen {
-      public:
-        InfiniPaint(DisplayApp* app, Pinetime::Components::LittleVgl& lvgl, Controllers::MotorController& motor);
+//       class InfiniPaint : public Screen {
+//       public:
+//         InfiniPaint(DisplayApp* app, Pinetime::Components::LittleVgl& lvgl, Controllers::MotorController& motor);
 
-        ~InfiniPaint() override;
+//         ~InfiniPaint() override;
 
-        bool OnTouchEvent(TouchEvents event) override;
+//         bool OnTouchEvent(TouchEvents event) override;
 
-        bool OnTouchEvent(uint16_t x, uint16_t y) override;
+//         bool OnTouchEvent(uint16_t x, uint16_t y) override;
 
-      private:
-        Pinetime::Components::LittleVgl& lvgl;
-        Controllers::MotorController& motor;
-        static constexpr uint16_t width = 10;
-        static constexpr uint16_t height = 10;
-        static constexpr uint16_t bufferSize = width * height;
-        lv_color_t b[bufferSize];
-        lv_color_t selectColor = LV_COLOR_WHITE;
-        uint8_t color = 2;
-      };
-    }
-  }
-}
+//       private:
+//         Pinetime::Components::LittleVgl& lvgl;
+//         Controllers::MotorController& motor;
+//         static constexpr uint16_t width = 10;
+//         static constexpr uint16_t height = 10;
+//         static constexpr uint16_t bufferSize = width * height;
+//         lv_color_t b[bufferSize];
+//         lv_color_t selectColor = LV_COLOR_WHITE;
+//         uint8_t color = 2;
+//       };
+//     }
+//   }
+// }
